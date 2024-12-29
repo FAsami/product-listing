38bebf5cf7af61378bc5a3b210c1d466ceae70b2
FASami <foysal.developer@gmail.com>
2024-12-29 13:09:36 +0600

    feat: add new SVG icons (Eye, Heart, Plus, ShoppingCart, Trash) and update index export

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+import { ShoppingCartIcon } from './ShoppingCart'
+import { EyeIcon } from './Eye'
+import { HeartIcon } from './Heart'
+import { PlusIcon } from './Plus'
+import { TrashIcon } from './Trash'
+
+export { ShoppingCartIcon, EyeIcon, HeartIcon, PlusIcon, TrashIcon }
